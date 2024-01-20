275fa56f53090f1958bbfeb60952d8d3aa7f2df9
vladosantic <vladosantic@hotmail.com>
2024-01-20 22:20:49 +0100

    Add filter layout and logic

--- app/src/main/java/ba/sum/fsre/nramu_project/fragment/PostsFragment.java
+++ app/src/main/java/ba/sum/fsre/nramu_project/fragment/PostsFragment.java
@@ -1,5 +1,7 @@
 package ba.sum.fsre.nramu_project.fragment;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,9 +12,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.ObservableSnapshotArray;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import ba.sum.fsre.nramu_project.R;
 import ba.sum.fsre.nramu_project.adapter.PostAdapter;
@@ -21,44 +32,80 @@ import ba.sum.fsre.nramu_project.Model.Post;
 
 public class PostsFragment extends Fragment {
 
-    FirebaseDatabase postsDatabase = FirebaseDatabase.getInstance();
-    PostAdapter postAdapter;
-    RecyclerView postRecyclerView;
-
+    private FirebaseDatabase postsDatabase;
+    private PostAdapter postAdapter;
+    private RecyclerView postRecyclerView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_posts, container, false);
-        this.postRecyclerView = view.findViewById(R.id.postListView);
-        this.postRecyclerView.setLayoutManager(
-                new LinearLayoutManager(getContext())
-        );
+
+        postsDatabase = FirebaseDatabase.getInstance();
+        postRecyclerView = view.findViewById(R.id.postListView);
+        postRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
         FirebaseRecyclerOptions<Post> options = new FirebaseRecyclerOptions.Builder<Post>().setQuery(
-                this.postsDatabase.getReference("posts"),
+                postsDatabase.getReference("posts"),
                 Post.class
         ).build();
 
-        this.postAdapter = new PostAdapter(options,this);
-        this.postRecyclerView.setAdapter(this.postAdapter);
+        postAdapter = new PostAdapter(options, this);
+        postRecyclerView.setAdapter(postAdapter);
 
+        // Add an OnClickListener to the filterIcon
+        ImageView filterIcon = view.findViewById(R.id.filterIcon);
+        filterIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSortingOptionsDialog(requireContext());
+            }
+        });
 
-        // Inflate the layout for this fragment
         return view;
     }
-
-
 
     @Override
     public void onStart() {
         super.onStart();
-        this.postAdapter.startListening();
+        postAdapter.startListening();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        this.postAdapter.stopListening();
+        postAdapter.stopListening();
+    }
+
+    public void showSortingOptionsDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        View sortingOptionsView = getLayoutInflater().inflate(R.layout.filter_dialog, null);
+
+        RadioGroup sortingOptionsGroup = sortingOptionsView.findViewById(R.id.sortingOptionsGroup);
+        Button applyButton = sortingOptionsView.findViewById(R.id.applyButton);
+
+        builder.setView(sortingOptionsView);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        applyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int selectedRadioButtonId = sortingOptionsGroup.getCheckedRadioButtonId();
+
+                if (selectedRadioButtonId == R.id.radioOldest) {
+                    postRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                }
+                if (selectedRadioButtonId == R.id.radioNewest) {
+                    LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+                    layoutManager.setReverseLayout(true);
+                    layoutManager.setStackFromEnd(true);
+                    postRecyclerView.setLayoutManager(layoutManager);
+                }
+
+                dialog.dismiss();
+            }
+        });
     }
 }
