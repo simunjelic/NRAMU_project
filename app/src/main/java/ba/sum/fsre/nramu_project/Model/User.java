a1547a468c905d7e09ee4c33f30d81f47b513754
vladosantic <vladosantic@hotmail.com>
2024-01-21 21:37:16 +0100

    Add User model

---
+++ app/src/main/java/ba/sum/fsre/nramu_project/Model/User.java
@@ -0,0 +1,41 @@
+package ba.sum.fsre.nramu_project.Model;
+
+public class User {
+    public String name;
+    public String lastName;
+    public String phoneNumber;
+
+    public User() {
+    }
+
+    public User(String name, String lastName, String phoneNumber) {
+        this.name = name;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
+
